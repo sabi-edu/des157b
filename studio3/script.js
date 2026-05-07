f093a0e059788d65b5b3a18ea96ebf91f6357112
sabi-edu <spatterson@ucdavis.edu>
2026-05-06 23:08:18 -0700

    studio3 v1

---
+++ studio3/script.js
@@ -0,0 +1,39 @@
+(function(){
+    'use strict';
+    console.log('reading js');
+
+    const granimInstance = new Granim({
+    element: '#granim-canvas',
+    name: 'granim',
+    opacity: [1, 1],
+    states : {
+        "default-state": {
+            gradients: [
+                ['#DCD9FF', '#E6D9FE'],
+                ['#F3DAFE', '#FDDAFD']
+            ]
+        }
+    }
+    });
+
+    const form = document.querySelector('form');
+    const input = document.querySelector('#input')
+    const submitBtn = document.querySelector('#submit')
+    const responseArea = document.querySelector('#response-area');
+    
+    submitBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        var value = input.value;
+        localStorage.setItem('userResponse', value);
+        pageSwitch();
+        responseArea.textContent = localStorage.getItem('userResponse');
+    });
+
+    function pageSwitch () {
+        form.remove();
+        $('#granim-canvas').removeClass('hidden').addClass('showing');
+        $('main').removeClass('hidden').addClass('showing');
+        $('#flower').removeClass('hidden').addClass('showing');
+    }
+
+})();
