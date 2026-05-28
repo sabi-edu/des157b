1c09c6b17b41839a03048d143b4bd17d146e64c6
Sabi Iniguez <sabinaif9@gmail.com>
2026-05-27 20:58:46 -0700

    usability vers

---
+++ capstone/usability/script.js
@@ -0,0 +1,42 @@
+(function(){
+    'use strict';
+    console.log('reading js');
+
+    const overlay = document.querySelector('#overlay')
+    const closeBtn = document.querySelector('#close-btn')
+    const form = document.querySelector('form');
+    const input = document.querySelector('#input')
+    const submitBtn = document.querySelector('#submit')
+    const msg = document.querySelector('#message');
+    
+
+    closeBtn.addEventListener('click', function(event){
+        event.preventDefault();
+        overlay.className = 'hidden';
+    });
+
+    document.addEventListener('keydown', function(event){
+        if (event.key === 'Escape') {
+            overlay.className = 'hidden';
+        }
+    });
+
+
+
+    submitBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        var value = input.value;
+        localStorage.setItem('userResponse', value);
+        pageSwitch();
+        msg.innerHTML += localStorage.getItem('userResponse');
+    });
+
+    function pageSwitch () {
+        form.remove();
+        $('#prompt').addClass('hidden');
+        $('#granim-canvas').removeClass('hidden').addClass('showing');
+        $('main').removeClass('hidden').addClass('showing');
+        $('#flower').removeClass('hidden').addClass('showing');
+    }
+
+})();
