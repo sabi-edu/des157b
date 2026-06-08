45e5f739079ed63d40918552374bdc06da84d304
Sabi Iniguez <sabinaif9@gmail.com>
2026-06-08 14:50:14 -0700

    final cp draft 1

--- capstone/project/script.js
+++ capstone/project/script.js
@@ -5,16 +5,34 @@
     Parse.initialize("1QKtkn4q7gLaOTJdxXdissUxzPOkC13MvZYHlQan", "bJyiDdLnA39prJznScm3eLr1fosjvSUxv1Q0frrD");
     Parse.serverURL = 'https://parseapi.back4app.com';
 
-    const overlay = document.querySelector('#overlay')
-    const closeBtn = document.querySelector('#close-btn')
-    const myForm = document.querySelector('form');
+    // --- DECLARATIONS ---
+    const overlay = document.querySelector('#overlay');
+    const closeBtn = document.querySelector('#close-btn');
+    const infoBtn = document.querySelector('#info-btn');
+    const header = document.querySelector('header');
+    const formFlower = document.querySelector('#form-flower')
+    const form = document.querySelector('form');
     let user;
     let html = '';
-    const submitBtn = document.querySelector('#submit-btn');
-    const letters = document.querySelector('#letters');   
+    const mainTag = document.querySelector('main');
+    const letterTag = document.querySelector('#letter');
+    const backBtn = document.querySelector('#back-btn');
+    const letterFlower1 = document.querySelector('#letter-flower1');
+    const letterFlower2 = document.querySelector('#letter-flower2');
+    const footer = document.querySelector('footer');
     
-    
+
+
+    // ------------------------------ FUNCTIONS
+
+
+    // --- OVERLAY ---
     closeBtn.addEventListener('click', function(event){
+        event.preventDefault();
+        overlay.className = 'hidden';
+    });
+
+    !overlay.addEventListener('click', function(event){
         event.preventDefault();
         overlay.className = 'hidden';
     });
@@ -25,28 +43,39 @@
         }
     });
 
-
-    myForm.addEventListener('submit', function(event) {
+    infoBtn.addEventListener('click', function(event){
         event.preventDefault();
+        overlay.className = 'showing';
+    });
 
+
+    // --- FORM ---
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
         addRecord();
-        pageSwitch();
+        formFlower.className = 'hidden';
+        form.className = 'hidden';
+        infoBtn.className = 'hidden';
+        header.className = 'hidden';
+        footer.className = 'hidden';
+        mainTag.className = 'showing';
+        letterFlower1.className = 'showing';
+        letterFlower2.className = 'showing';
     })
-
 
     async function addRecord() {
         const emailData = document.querySelector('#email');
-        const commentData = document.querySelector('#comments');
+        const messageData = document.querySelector('#message');
         const newObject = new Parse.Object('Letters');
-
-        user = emailData.ariaValueMax;
-        newObject.set('email', emailData.value);
-        newObject.set('comment', commentData.value);
         
+        user = emailData.value;
+        console.log(user);
+        newObject.set('email', emailData.value);
+        newObject.set('message', messageData.value);
         try {
             const result = await newObject.save();
             emailData.value = '';
-            commentData.value = '';
+            messageData.value = '';
             getLetters();
         } catch(error) {
             console.error('Error while creating ParseObject: ', error);
@@ -57,27 +86,27 @@
         const query = new Parse.Query('Letters');
         query.equalTo('email', user);
         query.descending('createdAt');
-
         try {
             const results = await query.find();
             for (const object of results) {
+                console.log(results);
                 const thisEmail = object.get('email');
-                const thisComment = object.get('comment');
-                html += `<article><p>${thisComment} from ${thisEmail}</p></article>`;
+                const thisMessage = object.get('message');
+                html += `<p><em>${thisMessage}</em></p>`;
+                console.log(`thisMessage is from ${thisEmail}`);
             }
-            letters.innerHTML = html;
+            letterTag.innerHTML = html;
+            console.log(html);
         } catch(error) {
-            console.error('Error while fetching Letters', error)
+            console.error('Error while fetching Letter', error);
         }
+        letterTag.style.addClass = 'letter';
     } 
 
-    
-    function pageSwitch () {
-        myForm.remove();
-        $('#prompt').addClass('hidden');
-        $('#granim-canvas').removeClass('hidden').addClass('showing');
-        $('main').removeClass('hidden').addClass('showing');
-        $('#flower').removeClass('hidden').addClass('showing');
-    }
+
+    // --- BACK BUTTON ---
+    backBtn.addEventListener('click', function(event){
+        location.reload();
+    });
 
 })();
