8db932fe8feb66b3725b69b686f3d3ef68ca4bb5
Sabi Iniguez <sabinaif9@gmail.com>
2026-06-08 17:56:07 -0700

    case study

--- capstone/development/version2/script.js
+++ capstone/development/version2/script.js
@@ -2,24 +2,10 @@
     'use strict';
     console.log('reading js');
 
-    const overlay = document.querySelector('#overlay')
-    const closeBtn = document.querySelector('#close-btn')
     const form = document.querySelector('form');
     const input = document.querySelector('#input')
     const submitBtn = document.querySelector('#submit')
     const msg = document.querySelector('#message');
-    
-
-    closeBtn.addEventListener('click', function(event){
-        event.preventDefault();
-        overlay.className = 'hidden';
-    });
-
-    document.addEventListener('keydown', function(event){
-        if (event.key === 'Escape') {
-            overlay.className = 'hidden';
-        }
-    });
 
 
     submitBtn.addEventListener('click', function(event) {
